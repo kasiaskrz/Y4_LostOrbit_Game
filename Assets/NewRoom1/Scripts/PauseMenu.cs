b3f5faf439a733de056fa6ba2edac3f372fa2d58
SAULESMALL <114486250+SAULESMALL@users.noreply.github.com>
2026-04-18 19:35:22 +0100

    Pause menu UI fix for Room01 and Room02

--- Assets/NewRoom1/Scripts/PauseMenu.cs
+++ Assets/NewRoom1/Scripts/PauseMenu.cs
@@ -85,7 +85,7 @@ public class PauseMenu : MonoBehaviour
         if (optionsPanel) optionsPanel.SetActive(true);
     }
 
-    public void CloseOptions()  // changed from private to public
+    public void CloseOptions()
     {
         if (optionsPanel) optionsPanel.SetActive(false);
         menuContainer.SetActive(true);
